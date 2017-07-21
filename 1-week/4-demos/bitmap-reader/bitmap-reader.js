d3e9145375c749cb62ffaf1df04911d41ee8b434
ohjonah <ohjonah@gmail.com>
2017-07-21 09:02:56 -0700

    End of Class Commit

--- 1-week/4-demos/bitmap-reader/bitmap-reader.js
+++ 1-week/4-demos/bitmap-reader/bitmap-reader.js
@@ -8,5 +8,8 @@ bmp.type = bitmap.toString('utf-8', 0, 2);
 bmp.size = bitmap.readInt32LE(2);
 bmp.width = bitmap.readInt32LE(18);
 bmp.height = bitmap.readInt32LE(22);
+bmp.colorArray = bitmap.readInt32LE(24);
+bmp.colorsInTable = bitmap.readInt32LE(46);
+bmp.importantColors = bitmap.readInt32LE(50);
 
 console.log('my current bitmap:', bmp);
